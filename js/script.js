b7689d09c1fc676b4709f3c143156f9365eeddb3
Samalamadoit <baglayfrilance@gmail.com>
2021-02-21 19:03:44 +0500

    ready site 3

--- js/script.js
+++ js/script.js
@@ -1,33 +1,56 @@
-(function () {
-    'use strict';
 
-    var boxElem = document.getElementById('box1');
-    var pointerElem = document.getElementById('pointer1');
+/*
+Simple Image Trail script- By JavaScriptKit.com
+Visit http://www.javascriptkit.com for this script and more
+This notice must stay intact
+*/
 
-    function onMouseMove(event) {
-        var mouseX = event.pageX;
-        var mouseY = event.pageY;
-        var crd = boxElem.getBoundingClientRect();
-        var activePointer = crd.left <= mouseX && mouseX <= crd.right && crd.top <= mouseY && mouseY <= crd.bottom;
+var trailimage=["img/207279_normal.png", 50, 50] //image path, plus width and height
+var offsetfrommouse=[10,-20] //image x,y offsets from cursor position in pixels. Enter 0,0 for no offset
+var displayduration=0 //duration in seconds image should remain visible. 0 for always.
 
-        requestAnimationFrame(function movePointer() {
-            if (activePointer) {
-                pointerElem.classList.remove('box-pointer-hidden');
-                pointerElem.style.left = Math.floor(mouseX) + 'px';
-                pointerElem.style.top = Math.floor(mouseY) + 'px';
-            } else {
-                pointerElem.classList.add('box-pointer-hidden');
-            }
-        });
+if (document.getElementById || document.all)
+    document.write('<div id="trailimageid" style="position:absolute;visibility:visible;left:0px;top:0px;width:1px;height:1px; z-index: 100000"><img src="'+trailimage[0]+'" border="0" width="'+trailimage[1]+'px" height="'+trailimage[2]+'px"></div>')
+
+function gettrailobj(){
+    if (document.getElementById)
+        return document.getElementById("trailimageid").style
+    else if (document.all)
+        return document.all.trailimagid.style
+}
+
+function truebody(){
+    return (!window.opera && document.compatMode && document.compatMode!="BackCompat")? document.documentElement : document.body
+}
+
+function hidetrail(){
+    gettrailobj().visibility="hidden"
+    document.onmousemove=""
+
+}
+
+function followmouse(e){
+    var xcoord=offsetfrommouse[0]
+    var ycoord=offsetfrommouse[1]
+    if (typeof e != "undefined"){
+        xcoord+=e.pageX
+        ycoord+=e.pageY
     }
-
-    function disablePointer() {
-        requestAnimationFrame(function hidePointer() {
-            pointerElem.classList.add('box-pointer-hidden');
-        });
+    else if (typeof window.event !="undefined"){
+        xcoord+=truebody().scrollLeft+event.clientX
+        ycoord+=truebody().scrollTop+event.clientY
     }
+    var docwidth=document.all? truebody().scrollLeft+truebody().clientWidth : pageXOffset+window.innerWidth-15
+    var docheight=document.all? Math.max(truebody().scrollHeight, truebody().clientHeight) : Math.max(document.body.offsetHeight, window.innerHeight)
+    if (xcoord+trailimage[1]+3>docwidth || ycoord+trailimage[2]> docheight)
+        gettrailobj().display="none"
+    else
+        gettrailobj().display=""
+    gettrailobj().left=xcoord+"px"
+    gettrailobj().top=ycoord+"px"
+}
 
-    boxElem.addEventListener('mousemove', onMouseMove, false);
-    boxElem.addEventListener('mouseleave', disablePointer, false);
+document.onmousemove=followmouse
 
-})();
+if (displayduration>0)
+    setTimeout("hidetrail()", displayduration*1000)
